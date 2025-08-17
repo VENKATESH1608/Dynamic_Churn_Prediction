fd8ef1911e39786ce0f5219c17aa0876ec0eb4f5
VENKATESH1608 <sankuvenkatesh9@gmail.com>
2025-08-18 01:13:08 +0530

    Add files via upload

---
+++ predict_your_customer/urls.py
@@ -0,0 +1,34 @@
+
+from django.contrib import admin
+from admins import views as admins
+from django.urls import path
+from users import views as usr
+from . import views as mainView
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', mainView.index, name='index'),
+    path("UserRegister/", mainView.UserRegister, name="UserRegister"),
+    path("AdminLogin/", mainView.AdminLogin, name="AdminLogin"),
+    path('index/', mainView.index, name='index'),
+    path("UserLogin/", mainView.UserLogin, name="UserLogin"),
+    
+    path("Viewdata/", mainView.Viewdata, name="Viewdata"),
+
+    ### User Side Views
+    path("UserRegisterActions/", usr.UserRegisterActions, name="UserRegisterActions"),
+    path("UserLoginCheck/", usr.UserLoginCheck, name="UserLoginCheck"),
+    path("UserHome/", usr.UserHome, name="UserHome"),
+
+    ### Admin Side Views
+    path("AdminLoginCheck/", admins.AdminLoginCheck, name="AdminLoginCheck"),
+    path("AdminHome/", admins.AdminHome, name="AdminHome"),
+    path("ViewRegisteredUsers/", admins.ViewRegisteredUsers, name="ViewRegisteredUsers"),
+    path("AdminActivaUsers/", admins.AdminActivaUsers, name="AdminActivaUsers"),
+    # path("AdminDataPreProcess/", admins.AdminDataPreProcess, name="AdminDataPreProcess"),
+    
+    path("UserTestRecordResult/", usr.UserTestRecordResult, name="UserTestRecordResult"),
+    path("UserSVM/", usr.UserSVM, name="UserSVM"),
+    path("UserChurn/",usr.UserChurn, name = "UserChurn")
+    #path("SVMResult/",usr.SVMResult,name = 'SVMResult')
+]
